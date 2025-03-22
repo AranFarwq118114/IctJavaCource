670dfcb0e3f8c8118112b530868460c9a46e01f3
AranFarwq118114 <[arankurd007@gmail.com]>
2025-03-23 02:49:32 +0300

    baxy mally karbeen (23/3/2025)

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Kak meer");
-
+        System.out.println("em shaw lagal jama3aty kolana la abaxy maly karbeenin la barwary 23/3/2025");
     }
 }
